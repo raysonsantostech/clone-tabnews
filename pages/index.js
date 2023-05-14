a936e63d2e6d0b0924ff6518d96abdedde3204dc
Rayson Santos <103448947+raysonsantostech@users.noreply.github.com>
2023-05-14 05:20:07 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ function Home() {
 
     <h3>Acho tão incrível quando após uma viagem de dez, quinze minutos até a creche você chega em casa e diz: amor, hoje tem história do Ícaro... essas histórias sempre terminam com boas risadas. É por essas e outras histórias que você e o nosso pequenino são as pessoas que mais importam e as mais especiais da minha vida. Você não faz ideia do tamanho da admiração, carinho e respeito que sinto por você e de como construir a nossa família junto de você me faz muito muito muito feliz. Sabe eu só quero te dizer que eu te amo… porque te amo, porque desde do nosso primeiro encontro na praia, desde do nosso primeiro abraço na areia da praia é o que eu sinto até hoje e quero sentir por toda a minha vida.
     </h3>
+    <em>Amo você</em>
   </div>
 }
 
